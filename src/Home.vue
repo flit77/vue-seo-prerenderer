9d5aadfc5629f864bac6b71340e078b07e7209e6
Vasiliy Shubko <flit77@gmail.com>
2017-04-15 01:41:54 +0200

    vue templates added

---
+++ src/Home.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    div(class="products")
+        div(v-for="product in products", class="product")
+            product(:product="product")
+</template>
+
+<script>
+import Product from './../components/Product.vue'
+
+export default {
+  name: 'home',
+  components: { Product },
+  computed: {
+    products(){
+      return this.$store.state.products
+    }
+  }
+}
+</script>
