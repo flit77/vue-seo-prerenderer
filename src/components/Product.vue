7dbf92a6ab8320fde6c26a851e7f5f12648735e6
Vasiliy Shubko <flit77@gmail.com>
2017-04-15 02:01:42 +0200

    restructured

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -4,13 +4,13 @@
       img(v-bind:src="product.image" v-bind:alt="product.name" class="thumbnail" height="200")
       p {{ product.name }}
     
-    button(class="snipcart-add-item"
-      v-bind:data-item-name="product.name"
-      v-bind:data-item-id="product.id"
-      v-bind:data-item-image="product.image"
-      data-item-url="/"
-      v-bind:data-item-price="product.price")
-        | Buy it for {{ product.price }}$
+   button(class="snipcart-add-item"
+     v-bind:data-item-name="product.name"
+     v-bind:data-item-id="product.id"
+     v-bind:data-item-image="product.image"
+     data-item-url="/"
+     v-bind:data-item-price="product.price")
+       | Buy it for {{ product.price }}$
  
 </template>
 
